2f442b1405a0e0d19211e139d1fd5ca7653828bd
rebeccaaaaaaaaaaa <rebeccaignacio@outlook.com>
2021-11-20 16:12:41 -0300

    mudança da frase da tela de intro

--- src/components/ChatIntro.js
+++ src/components/ChatIntro.js
@@ -6,7 +6,7 @@ export default function ChatIntro() {
     return (
         <div className="chatIntro">
           <img src="/intro.svg" alt=""/>
-          <h1> Esteja logado no Facebook</h1>
+          <h1> Selecione um chat para começar a conversar</h1>
           <h2> A aplicação utiliza seu nome e foto do facebook.</h2>
         </div>
     )
